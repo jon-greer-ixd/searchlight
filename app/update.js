584778b6a935a52acd27e36ba4184441e58978e4
Jon Greer <jongreer@Sams-MacBook-Pro.local>
2018-02-26 14:32:43 +0000

    added name 2

---
+++ app/update.js
@@ -0,0 +1,48 @@
+// I need to update to a new address
+  // supply new address
+  // supply new status
+  // supply new dates
+
+// I need to correct to a new address
+  // supply new address
+  // supply new status
+
+// function to change one date to another
+// function to change one address to another
+// function to change one status to another
+// function to change one address line to another
+
+// Make an address previous (current - if current display As such)
+// Make an address residential or correspondence (current - if current display As such)
+
+
+
+
+
+// function to change one date (string) to another
+
+
+/* var Address = {
+  current = null;
+  residential = null;
+  startDate = null;
+  endDate = null;
+  cherrish = null;
+  cherrishedLine = null;
+  lineOne = null;
+  lineTwo = null;
+  lineThree = null;
+  lineFour = null;
+  lineFive = null;
+  status;
+}
+
+setJourney = {
+it
+}
+
+updateAddress(address) {
+  if changeType = x address.status = current;
+}
+
+*/
