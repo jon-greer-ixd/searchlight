b8a639de7cbf68f23d806f289d1bf35c3066f29d
Jon <jongreer.digitaldwp@gmail.com>
2018-10-24 15:30:27 +0100

    mc

--- functions/general.js
+++ functions/general.js
@@ -1,10 +1,20 @@
 // 1 ADD / 2 UPDATE / 3 CORRECT / 4 REMOVE / 5 END / 6 DELETE
+
 function setToasterMessage(item, type, state) {
   var message;
-  if (state == 1) {
+  if (state == 1 || state == "adding") {
     state = "added";
   }
-  if (state == 2) {
+  if (type == "phone" || type == "fax" || type == "textPhone" || type == "typeTalk" || type == "other-phone") {
+    type = "number";
+  }
+  if (type == "email") {
+    type = "address";
+  }
+  if (type == "other") {
+    type = "";
+  }
+  if (state == 2 || state == "updating") {
     state = "updated";
   }
   if (state == 3 || state == "correcting") {
@@ -19,12 +29,14 @@ function setToasterMessage(item, type, state) {
   if (state == 6) {
     state = "deleted";
   }
+  if (state == "removing") {
+    state = "ended";
+  }
   if (type == null) {
     message = `${item} ${state}`;
   } else {
     message = `${item} ${type} ${state}`;
   }
-  console.log(`message = ${message}`);
   return message
 }
 
